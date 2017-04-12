4998a2fc250c904daba2c846d85ed84a1546b0ef
Niklas Heer <niklas.heer@gmail.com>
2017-04-12 13:39:03 +0200

    adds support for generating repo index files

--- paci/commands/generate.py
+++ paci/commands/generate.py
@@ -1,7 +1,6 @@
 """The generate command."""
 
 import os
-import tarfile
 from .base import Base
 from paci.helpers import file_helper
 from paci.helpers.pkg_index import PkgIndex
@@ -11,36 +10,33 @@ class Generate(Base):
     """Generate index files!"""
 
     def run(self):
-        if self.options['pkg-index']:
+        if self.options["pkg-index"]:
             print("Regenerating the index file for all installed packages...\n")
 
-        if self.options['repo-index']:
-            repo_path = self.options['<path>']
+        if self.options["repo-index"]:
+            repo_path = self.options["<path>"]
             print("Generating the index file for all packages inside a repository...\n")
 
             if os.path.isabs(repo_path):
-                repo_db = PkgIndex(repo_path + "/index.json")
+                repo_file = os.path.join(repo_path, "index.json")
+
+                # Fresh new start
+                file_helper.safe_delete(repo_file)
+                repo_db = PkgIndex(repo_file)
 
                 for root, dirs, files in os.walk(repo_path):
-                    files = [f for f in files if not f[0] == '.']
-                    dirs[:] = [d for d in dirs if not d[0] == '.']
+                    files = [f for f in files if not f[0] == "."]
+                    dirs[:] = [d for d in dirs if not d[0] == "."]
 
-                    if 'RECIPE.yml' in files:
+                    if "RECIPE.yml" in files:
                         pkg_conf = file_helper.get_pkg_conf(root + "/RECIPE.yml")
                         repo_db.add({
-                            'pkg_ver': pkg_conf['version'],
-                            'pkg_desc': pkg_conf['summary'],
-                            'pkg_name': pkg_conf['name']
+                            "pkg_ver": pkg_conf["version"],
+                            "pkg_desc": pkg_conf["summary"],
+                            "pkg_name": pkg_conf["name"]
                         })
-                tar = os.path.join(repo_path, "index.json.gz")
-                print(tar)
-                #
-                # a + "," + b + "\nii"
-                # "%s, %d" % (a, b)
-                with tarfile.open(tar, "w:gz") as archive:
-                    archive.add(repo_path + "/index.json", arcname='.')
-                    # archive.addfile(tarfile.TarInfo("index.json.gz"), file("/path/to/filename.txt"))
 
+                print("Index file successfully written to: {}".format(repo_file))
             else:
                 print("Error! Please provide an absolute path to your repository!")
                 exit(1)
