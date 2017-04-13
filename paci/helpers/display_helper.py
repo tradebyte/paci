5ff03111862e5e82575ae29137952effd1777945
Niklas Heer <niklas.heer@gmail.com>
2017-04-13 15:54:39 +0200

    improves the output of tables

--- paci/helpers/display_helper.py
+++ paci/helpers/display_helper.py
@@ -1,15 +1,8 @@
 """Helper to output stuff"""
 
+from tabulate import *
+
 
 def print_list(header, entries):
     """Prints out a list"""
-
-    # Add the header
-    entries.insert(0, header)
-
-    # Get the width
-    col_width = max(len(word) for entry in entries for word in entry) + 2
-
-    # Print everything
-    for entry in entries:
-        print("".join(word.ljust(col_width) for word in entry))
+    print(tabulate(entries, header, tablefmt="grid"))
