2eb82e07b0868e471a6bab8ae86183b123d249a3
Niklas Heer <niklas.heer@gmail.com>
2017-06-07 13:25:40 +0200

    add configure command. fixes #18

--- paci/commands/configure.py
+++ paci/commands/configure.py
@@ -1,13 +1,39 @@
 """The configure command lets you define a settings.yml for paci."""
 
+import os
 from .base import Base
+from paci.helpers.settings import Settings
+from paci.helpers import display_helper
 
 
 class Configure(Base):
     """Creates a settings.yml for paci interactive with the user."""
 
     def run(self):
-        print("Not yet implemented!")
-        # print("Lets configure a new settings.yml for paci!")
-        # arg = input("basedir (defaults to $HOME/.paci/: ")
-        # print(arg)
+        print("Lets configure a new settings.yml for paci!\n")
+        settings_helper = Settings()
+
+        # get user input for all values
+        temp = display_helper.std_input("Enter directory to store temporary files ({}):\n",
+                                        settings_helper.defaults["paci"]["temp"])
+        base = display_helper.std_input("Enter file in which to save the package meta data ({}):\n",
+                                        settings_helper.defaults["paci"]["base"])
+        r_main = display_helper.std_input("Enter main registry url ({}):\n",
+                                          settings_helper.defaults["paci"]["registry"]["main"])
+        r_fallback = display_helper.std_input("Enter fallback registry url ({}):\n",
+                                              settings_helper.defaults["paci"]["registry"]["fallback"])
+
+        # create settings dict
+        settings = {
+            "paci": {
+                "temp": os.path.abspath(os.path.expanduser(temp)),
+                "base": os.path.abspath(os.path.expanduser(base)),
+                "registry": {
+                    "main": r_main,
+                    "fallback": r_fallback
+                }
+            }
+        }
+
+        # save the settings
+        settings_helper.write_settings(settings)
