7bf7b4a9ca42c854f4c0ce4fec98bbb8ab06497e
Niklas Heer <niklas.heer@gmail.com>
2017-06-07 13:16:26 +0200

    add helper function to get an input with a default setting

--- paci/helpers/display_helper.py
+++ paci/helpers/display_helper.py
@@ -6,3 +6,7 @@ from tabulate import *
 def print_list(header, entries):
     """Prints out a list"""
     print(tabulate(entries, header, tablefmt="grid"))
+
+
+def std_input(text, default):
+    return input(text.format(default)) or default
