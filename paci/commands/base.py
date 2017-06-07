462fd75598d52a3679b11c984bd148bda507f7d0
Niklas Heer <niklas.heer@gmail.com>
2017-06-07 13:17:57 +0200

    add a saver way to fix something if you messed up the settings.yml

--- paci/commands/base.py
+++ paci/commands/base.py
@@ -14,22 +14,23 @@ class Base(object):
         self.args = args
         self.kwargs = kwargs
 
-        settings_helper = Settings()
+        if not options["configure"]:
+            settings_helper = Settings()
 
-        if settings_helper.settings_exist() is False:
-            settings_helper.write_settings(settings_helper.defaults)
+            if settings_helper.settings_exist() is False:
+                settings_helper.write_settings(settings_helper.defaults)
 
-        # Define global variables
-        self.settings = settings_helper.fetch_settings()
-        self.repo_cache = os.path.join(self.settings["paci"]["base"], "cache")
-        self.base_pkg_dir = os.path.join(self.settings["paci"]["base"], "apps")
-        self.index_file = os.path.join(self.settings["paci"]["base"], "pkgs.json")
-        self.index = PkgIndex(self.index_file)
+            # Define global variables
+            self.settings = settings_helper.fetch_settings()
+            self.repo_cache = os.path.join(self.settings["paci"]["base"], "cache")
+            self.base_pkg_dir = os.path.join(self.settings["paci"]["base"], "apps")
+            self.index_file = os.path.join(self.settings["paci"]["base"], "pkgs.json")
+            self.index = PkgIndex(self.index_file)
 
-        # Ensure the directories are present
-        os.makedirs(self.repo_cache, exist_ok=True)
-        os.makedirs(self.base_pkg_dir, exist_ok=True)
-        os.makedirs(self.settings["paci"]["temp"], exist_ok=True)
+            # Ensure the directories are present
+            os.makedirs(self.repo_cache, exist_ok=True)
+            os.makedirs(self.base_pkg_dir, exist_ok=True)
+            os.makedirs(self.settings["paci"]["temp"], exist_ok=True)
 
     def run(self):
         raise NotImplementedError("You must implement the run() method yourself!")
