a6ad5e60feec888b26191f7c07a4bbeb8c335c07
Niklas Heer <niklas.heer@gmail.com>
2017-06-12 17:23:37 +0200

    add download url

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     description="Your friendly, lightweight and flexible package manager.",
     long_description=readme,
     url="https://github.com/tradebyte/paci",
+    download_url="https://github.com/tradebyte/paci/archive/v1.0.0.tar.gz",
     author="Niklas Heer",
     author_email="niklas.heer@tradebyte.com",
     license=license_file,
