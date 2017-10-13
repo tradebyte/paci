251a0d1b1df0fd857a86878ecb7e4c6bc26a93ef
Niklas Heer <niklas.heer@gmail.com>
2017-10-13 14:08:03 +0200

    Add function to just print a simple table

--- paci/helpers/display_helper.py
+++ paci/helpers/display_helper.py
@@ -8,6 +8,11 @@ def print_list(header, entries):
     print(tabulate(entries, header, tablefmt="grid"))
 
 
+def print_table(entries):
+    """Prints out a table"""
+    print(tabulate(entries, tablefmt="plain"))
+
+
 def std_input(text, default):
     """Get input or return default if none is given."""
     return input(text.format(default)) or default
