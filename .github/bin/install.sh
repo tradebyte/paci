78a3ae1283105659c72fc7803fd45cc6036b43c8
Niklas Heer <me@nheer.io>
2017-11-24 15:52:03 +0100

    Add easydict as dependency

--- .github/bin/install.sh
+++ .github/bin/install.sh
@@ -131,7 +131,7 @@ main() {
 
     # Define settings
     REQs="python3 python3-venv python3-pip rsync git"
-    PY_REQs=("paci" "halo" "xonsh" "termcolor")
+    PY_REQs=("paci" "halo" "xonsh" "termcolor" "easydict" "log_symbols")
 
     # Use colors, but only if connected to a terminal, and that terminal
     # supports them.
