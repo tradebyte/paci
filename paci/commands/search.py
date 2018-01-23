ca5877d66698179a3c26d12938dfac10cd19d932
Niklas Heer <niklas.heer@gmail.com>
2018-01-23 20:21:28 +0100

    Add a description of what the output represents

--- paci/commands/search.py
+++ paci/commands/search.py
@@ -1,6 +1,7 @@
 """The search command."""
 
-from paci.helpers import display_helper, cache_helper
+from paci.helpers import display_helper, cache_helper, std_helper
+from termcolor import colored
 from .base import Base
 
 
@@ -11,6 +12,7 @@ class Search(Base):
         pkg_name = self.options["<package>"]
         result = cache_helper.find_pkg(pkg_name, self.settings["paci"]["registry"], self.repo_cache)
         if result:
+            print(colored("Showing all search results for \"{}\"...\n".format(std_helper.stringify(pkg_name)), 'yellow', attrs=['bold']))
             display_helper.print_list(["Name", "Version", "Description", "Registry"], result)
         else:
             print("Package not found.")
